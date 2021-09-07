414f3d82787799bc8c8083a466cb2d8f764b1882
Catopuse <gbds234@gmail.com>
2021-09-08 00:35:21 +0900

    가장 긴 증가하는 부분 수열: o(n log n)

--- 14003.cpp
+++ 14003.cpp
@@ -1,60 +1,60 @@
 //14003.cpp
 
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
 int sequence[1000001];
+int idx[1000001];
 int arr[1000001];
-vector<int> candidate[1000001];
+int parent[1000001];
+int ans[1000001];
 
 int find_idx(int x,int start,int end){
     if(start==end)
         return start;
     int mid = (start+end)/2;
-    if(sequence[mid]>x){
+    if(sequence[mid]>=x){
         return find_idx(x,start,mid);
     }
-    else if(sequence[mid]<x){
-        return find_idx(x,mid+1,end);
-    }
     else{
-        return -1;
+        return find_idx(x,mid+1,end);
     }
 }
 
 int main(){
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL); 
+    cout.tie(NULL);
+
     int N;
     int a;
-    cin >> N;
+    scanf("%d",&N);
     int count = 0;
-    vector<int> ans;
-    for(int i=0;i<N;i++){
+    for(int i=1;i<=N;i++){
         scanf("%d",&a);
         int temp = find_idx(a,0,count);
         arr[i] = a;
-        if(temp == -1)
-            continue;
-        candidate[temp].push_back(a);
+
         sequence[temp] = a;
-        if(temp == count)
+        idx[temp] = i;
+        if(temp>0)
+            parent[i] = idx[temp-1];
+
+        if(temp == count){
             count++;
+        }
+
     }
     cout<<count<<"\n";
-
-    int big = candidate[count-1][0];
-    ans.push_back(big);
-    for(int i=count-2;i>=0;i--){
-        for(int j=0;j<candidate[i].size();j++){
-            if(candidate[i][j] < big){
-                big = candidate[i][j];
-                ans.push_back(big);
-                break;
-            }
-        }
+    int big = idx[count-1];
+    int idxs = count-1;
+    while(idxs>=0){
+        ans[idxs] = arr[big];
+        big = parent[big];
+        idxs--;
     }
-    for(int i=ans.size()-1;i>=0;i--)
+    for(int i=0;i<count;i++){
         cout<<ans[i]<<" ";
+    }
 }
