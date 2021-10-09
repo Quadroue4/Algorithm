497deb1bf62e0d42af0a22527e77bc42edacfcc2
Catopuse <gbds234@gmail.com>
2021-10-10 00:59:51 +0900

    dp,분할정복
    시중에 있는 코드에 dp를 가미함..

---
+++ 20047.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+int n, x, y;
+char S[10001], T[10001], fingers[2];
+int dp[10001][3];
+
+int findAnswer(int index, int fingerCnt){
+    if(index + fingerCnt == n) return 1;
+    
+    if(dp[index][fingerCnt] != -1)
+        return dp[index][fingerCnt];
+
+    if(S[index] == T[index + fingerCnt]){
+        if(findAnswer(index + 1, fingerCnt)) return dp[index][fingerCnt] = 1;
+    }
+    
+    if(fingerCnt == 2)return dp[index][fingerCnt] = 0;
+
+    if(fingers[fingerCnt] == T[index + fingerCnt]){
+        if(findAnswer(index, fingerCnt + 1)) return dp[index][fingerCnt] = 1;
+    }
+
+    return dp[index][fingerCnt] = 0;
+}
+
+
+int main(){
+    scanf("%d\n%s\n%s\n%d %d", &n, S, T, &x, &y);
+    fingers[0] = S[x], fingers[1] = S[y];
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<3;j++)
+            dp[i][j] = -1;
+    }
+    int t = 0;
+    for(int i = 0; i < n; i++){
+        if(i == x || i == y)continue;
+        S[t] = S[i];
+        t++;
+    }
+
+    printf("%s\n", findAnswer(0, 0) ? "YES" : "NO");
+}
